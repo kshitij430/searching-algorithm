f760c16178d8d1e72edf12f323ab9a6eee0fc2f0
kshitij430 <59320687+kshitij430@users.noreply.github.com>
2025-07-02 16:36:26 +0530

    package file updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App";
 console.log("STARTING");
 
+function runUserCode(userInput) {
+  eval(userInput); // Dangerous!
+}
+
+runUserCode("console.log('Hello!')");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
