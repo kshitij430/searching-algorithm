d67b8f1e7b6de86830835a745427b1425b8e0a08
kshitij430 <59320687+kshitij430@users.noreply.github.com>
2025-07-02 16:42:26 +0530

    package file updated

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
-export default function App({ userInput }) {
-  return <div dangerouslySetInnerHTML={{ __html: userInput }} />;
+export default function App(props) {
+  // Simulate user input from props (which is untrusted)
+  return <div dangerouslySetInnerHTML={{ __html: props.userInput }} />;
 }
